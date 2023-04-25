9b83022b662e3f1dfec7c0784e206f91a56b262f
Allan Milo <63133262+Allanmilo@users.noreply.github.com>
2023-04-24 20:05:25 -0400

    Collisions initial commit.

---
+++ Assets/Balls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Balls : MonoBehaviour
+{
+  GameObject theBalls;
+
+  Control_Script _control_Script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theBalls = GameObject.FindGameObjectWithTag("TheBalls");
+        _control_Script = theBalls.GetComponent<Control_Script>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  //  If you need to find out which of your child colliders was involved in the collision, you can do it like so:
+ 
+void OnCollisionEnter2D(Collision2D collision)
+{
+  if(collision.gameObject.tag == "Ball")
+  {
+
+    _control_Script.paddle.Add(gameObject);
+
+    // Collider2D myCollider = collision.GetContact(0).otherCollider;
+    // Debug.Log("this col is " + myCollider);
+    // Now do whatever you need with myCollider.
+    // (If multiple colliders were involved in the collision, 
+    // you can find them all by iterating through the contacts)
+  }
+}
+
+}
---
+++ Assets/Control_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Control_Script : MonoBehaviour
+{
+    public List<GameObject> paddle =  new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+            {
+                foreach (var card in paddle)
+{
+        Debug.Log(card);
+        
+            }
+        
+}
+    }
+}
